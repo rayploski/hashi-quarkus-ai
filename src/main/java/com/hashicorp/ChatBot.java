01fd43a4b28b4e874cdd6f96c2d8e820dae84662
Ray Ploski <ray.ploski@gmail.com>
2024-08-13 10:51:52 -0700

    Added ChatBot, RAG and sample data

---
+++ src/main/java/com/hashicorp/ChatBot.java
@@ -0,0 +1,16 @@
+package com.hashicorp;
+
+import dev.langchain4j.service.SystemMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
+import jakarta.enterprise.context.SessionScoped;
+
+@RegisterAiService(retrievalAugmentor = Retriever.class)
+@SystemMessage("""
+    You are Mona, a chatbot answering question about a museum. Be polite, concise and helpful.
+""")
+@SessionScoped
+public interface ChatBot {
+
+    String chat(String question);
+
+}
